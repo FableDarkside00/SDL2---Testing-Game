a380d40d3ec9d3b5d514e411eb60d933f28be5d7
FableDarkside00 <saymonmcedo00@gmail.com>
2024-07-03 14:47:11 -0300

    flags

--- compiler.bat
+++ compiler.bat
@@ -1,5 +1,4 @@
 @echo off
 Rem Alias do compilador para o GLFW
 gcc -o %1.exe .\%1.c -I C:\MinGW\include\SDL2 -L C:\MinGW\lib -lSDL2main -lSDL2 -DSDL_MAIN_HANDLED -lopengl32 
-
 Rem C:\GLFW\include\GLFW C:\GLFW\lib\libglfw3.a -lglfw3 -lgdi32 
